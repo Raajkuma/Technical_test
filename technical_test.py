1b9a81ce07aac1e7d5c7e5b897c0b9253ee20046
Raajkuma <105586677+Raajkuma@users.noreply.github.com>
2022-05-15 16:31:06 +0530

    technical_test.py file Uploaded

---
+++ technical_test.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May  4 20:10:35 2020
+
+@author:Raajkumar
+"""
+import re
+def add(string):
+    "Calculating the Sum of Total digit value from the string"
+    string=re.split(";|,", string)
+    sum_of_string_numbers=0
+    negative_values=[]
+    for i in string:
+        if i.lstrip('-').replace('.', '', 1).isdigit():
+            if int(i)>=0:
+                sum_of_string_numbers+=int(i)
+            else:
+                print()
+                negative_values.append(i)
+
+    if len(negative_values)>0:
+        raise Exception("negatives not allowed",",".join(negative_values))
+    return sum_of_string_numbers
+
+INPUT_STRING="1\n2,3"
+print(add(INPUT_STRING))
